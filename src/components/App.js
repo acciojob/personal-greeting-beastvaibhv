eabc2c70322abfaa8b348620fd959f8f908f98a9
Vaibhav Bisht <92950515+beastvaibhv@users.noreply.github.com>
2023-12-12 10:08:12 +0000

    personal greeting

--- src/components/App.js
+++ src/components/App.js
@@ -3,15 +3,16 @@ import React, {useState} from "react";
 import './../styles/App.css';
 
 const App = () => {
-  let [text, setText]  = useState("");
+  let [text, setText]  = useState('');
   return (
     <div>
+      <p>Enter your name:</p>
        <input
        onChange={(e)=>setText(e.target.value)}
        value={text}/>
-       {text.length > 0 ? <p>Hello {text}</p>: null }
+       {text.length > 0 ? <p>Hello {text}!</p>: null }
     </div>
   )
 }
 
-export default App
+export default App;
